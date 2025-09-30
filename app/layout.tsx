8fe63ea4187652d3dc3362e19f2bee2c2cbc14ac
borkeradarsh <borkeradi07@gmail.com>
2025-09-30 13:49:18 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import ScrollToTop from '@/components/ScrollToTop'
+import { Analytics } from '@vercel/analytics/next'
 
 export const metadata: Metadata = {
   title: 'Adarsh - Portfolio',
